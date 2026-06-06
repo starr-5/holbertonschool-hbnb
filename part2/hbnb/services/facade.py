2a892176b53333e69613f2f034c5bb86f2a5ff6d
starr-5 <studying12567@gmail.com>
2026-06-06 05:38:15 +0000

    task

--- part2/hbnb/services/facade.py
+++ part2/hbnb/services/facade.py
@@ -16,3 +16,14 @@ class HBnBFacade:
 
     def update_user(self, user_id, user):
         self.repo.update(user_id, user)
+    def create_amenity(self, amenity):
+    self.repo.add(amenity)
+
+    def get_amenity(self, amenity_id):
+    return self.repo.get(amenity_id)
+
+    def get_all_amenities(self):
+    return self.repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity):
+    self.repo.update(amenity_id, amenity)
