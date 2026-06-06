ae3023b76dad8d383247b38c7b5081866f575950
starr-5 <studying12567@gmail.com>
2026-06-06 05:33:02 +0000

    task

--- part2/hbnb/services/facade.py
+++ part2/hbnb/services/facade.py
@@ -1,2 +1,18 @@
+from hbnb.persistence.repository import InMemoryRepository
+
+
 class HBnBFacade:
-    pass
+
+    def __init__(self):
+        self.repo = InMemoryRepository()
+            def create_user(self, user):
+        self.repo.add(user)
+
+    def get_user(self, user_id):
+        return self.repo.get(user_id)
+
+    def get_all_users(self):
+        return self.repo.get_all()
+
+    def update_user(self, user_id, user):
+        self.repo.update(user_id, user)
